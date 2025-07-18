1cd91a66b616d8d1a5558a5b0b18100bce0b0847
seucaio <caiopimentel8@gmail.com>
2025-07-18 03:03:47 -0300

    feat: Add ExchangeListScreen and ExchangeDetailsScreen
    This commit introduces two new Composable screens:
    
    - `ExchangeListScreen`: A basic Composable function intended for displaying a list of exchanges. It includes a `@Preview` for development and testing.
    - `ExchangeDetailsScreen`: A basic Composable function intended for displaying the details of a specific exchange. It also includes a `@Preview`.
    
    Both screens are currently placeholders with no specific UI elements implemented yet.

---
+++ app/src/main/java/br/com/seucaio/cryptoexchanges/ui/screen/details/ExchangeDetailsScreen.kt
@@ -0,0 +1,18 @@
+package br.com.seucaio.cryptoexchanges.ui.screen.details
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ExchangeDetailsScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+private fun PreviewExchangeDetailsScreen() {
+    ExchangeDetailsScreen()
+}
---
+++ app/src/main/java/br/com/seucaio/cryptoexchanges/ui/screen/list/ExchangeListScreen.kt
@@ -0,0 +1,18 @@
+package br.com.seucaio.cryptoexchanges.ui.screen.list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ExchangeListScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+private fun PreviewExchangeListScreen() {
+    ExchangeListScreen()
+}
