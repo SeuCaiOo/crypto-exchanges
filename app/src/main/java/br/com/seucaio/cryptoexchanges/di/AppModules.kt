03a814613c40f5a5672c62a53180eacfdb7cf523
seucaio <caiopimentel8@gmail.com>
2025-07-18 04:19:09 -0300

    feat: Configure Koin modules and initialize in Application
    This commit introduces Koin modules for dependency injection and initializes Koin in the `MyApplication` class.
    
    Key changes:
    - Created `AppModules.kt` which defines Koin modules for network, data, use case, and ViewModel layers.
      - `networkModule`: Provides dependencies for network operations including `HttpLoggingInterceptor`, `OkHttpClient`, `Retrofit`, and `ApiService`.
      - `dataModule`: Provides `ExchangeDataSource` and `ExchangeRepository`.
      - `useCaseModule`: Provides `GetExchangesUseCase`.
      - `viewModelModule`: Provides `ExchangeListViewModel`.
    - Updated `MyApplication.kt` to initialize Koin on application startup using `startKoin`. It configures `androidLogger`, `androidContext`, and loads the defined `appModules`.

---
+++ app/src/main/java/br/com/seucaio/cryptoexchanges/di/AppModules.kt
@@ -0,0 +1,39 @@
+package br.com.seucaio.cryptoexchanges.di
+
+import br.com.seucaio.cryptoexchanges.data.repository.ExchangeRepositoryImpl
+import br.com.seucaio.cryptoexchanges.data.service.ApiService
+import br.com.seucaio.cryptoexchanges.data.source.ExchangeDataSource
+import br.com.seucaio.cryptoexchanges.domain.repository.ExchangeRepository
+import br.com.seucaio.cryptoexchanges.domain.usecase.GetExchangesUseCase
+import br.com.seucaio.cryptoexchanges.ui.screen.list.ExchangeListViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+val networkModule = module {
+    single<HttpLoggingInterceptor> { NetworkProvider.loggingInterceptor() }
+    single<OkHttpClient> {
+        NetworkProvider.okHttpClient(interceptors = listOf(get<HttpLoggingInterceptor>()))
+    }
+    single<Retrofit> { NetworkProvider.providesRetrofit(okHttpClient = get<OkHttpClient>()) }
+    single<ApiService> { get<Retrofit>().create(ApiService::class.java) }
+}
+
+val dataModule = module {
+    single<ExchangeDataSource> { ExchangeDataSource(apiService = get<ApiService>()) }
+    single<ExchangeRepository> { ExchangeRepositoryImpl(dataSource = get<ExchangeDataSource>()) }
+}
+
+val useCaseModule = module {
+    single<GetExchangesUseCase> { GetExchangesUseCase(repository = get<ExchangeRepository>()) }
+}
+
+val viewModelModule = module {
+    viewModel<ExchangeListViewModel> {
+        ExchangeListViewModel(getExchangesUseCase = get<GetExchangesUseCase>())
+    }
+}
+
+val appModules = listOf(networkModule, dataModule, useCaseModule, viewModelModule)
