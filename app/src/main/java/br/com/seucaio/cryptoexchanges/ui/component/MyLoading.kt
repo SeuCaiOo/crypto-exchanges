0b98f2966397e353ef33f1bfc9612f11e9a0c891
seucaio <caiopimentel8@gmail.com>
2025-07-19 01:51:26 -0300

    feat: Add MyError and MyLoading Composable components
    This commit introduces two new UI components: `MyError` and `MyLoading`.
    
    Key changes:
    - Created `MyError` Composable:
        - Displays a customizable error message with a title and a retry button.
        - Uses `MaterialTheme` for typography.
        - Includes `@Preview` for light and dark modes, and for custom error messages.
    - Created `MyLoading` Composable:
        - Displays a centered `CircularProgressIndicator`.
        - Includes `@Preview` for light and dark modes.

---
+++ app/src/main/java/br/com/seucaio/cryptoexchanges/ui/component/MyLoading.kt
@@ -0,0 +1,40 @@
+package br.com.seucaio.cryptoexchanges.ui.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.seucaio.cryptoexchanges.ui.CryptoExchangeSurface
+
+@Composable
+fun MyLoading(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = modifier,
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewMyLoading() {
+    CryptoExchangeSurface {
+        MyLoading()
+    }
+}
