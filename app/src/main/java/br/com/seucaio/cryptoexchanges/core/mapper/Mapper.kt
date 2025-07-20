6c7d006906701d8a64d287ed9fa8d1b3c4281c72
seucaio <caiopimentel8@gmail.com>
2025-07-20 03:25:21 -0300

    refactor: Update Mapper interfaces
    This commit renames `MapperResponse.kt` to `Mapper.kt` and introduces a new generic `Mapper` interface.
    
    Key changes:
    - Renamed `core/mapper/MapperResponse.kt` to `core/mapper/Mapper.kt`.
    - Introduced a new `Mapper<R, L, T>` interface with methods for mapping between remote, local, and domain models:
        - `mapLocalToDomain(source: L): T`
        - `mapRemoteToDomain(source: R): T`
        - `mapRemoteToEntity(source: R): L`
        - `mapDomainToEntity(source: T): L`
    - The existing `MapperResponse` and `MapperEntity` interfaces remain in the new `Mapper.kt` file.

--- app/src/main/java/br/com/seucaio/cryptoexchanges/core/mapper/Mapper.kt
+++ app/src/main/java/br/com/seucaio/cryptoexchanges/core/mapper/Mapper.kt
@@ -8,3 +8,10 @@ interface MapperEntity<S, T> {
     fun mapToEntity(source: S): T
     fun mapToDomain(source: T): S
 }
+
+interface Mapper<R, L, T> {
+    fun mapLocalToDomain(source: L) : T
+    fun mapRemoteToDomain(source: R): T
+    fun mapRemoteToEntity(source: R) : L
+    fun mapDomainToEntity(source: T) : L
+}
