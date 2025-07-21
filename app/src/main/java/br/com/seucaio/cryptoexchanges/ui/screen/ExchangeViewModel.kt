880095d90dc28ce41a089b6039d58bfa1a342a82
seucaio <caiopimentel8@gmail.com>
2025-07-21 00:58:12 -0300

    feat: Enhance ExchangeViewModel with refresh and error handling
    This commit updates `ExchangeViewModel` to include pull-to-refresh functionality and improved error handling, including displaying Snackbars for warnings.
    
    Key changes:
    - Added `SnackbarHostState` to `ExchangeViewModel` for displaying messages.
    - Modified `getExchanges` to accept a `forceRefresh` parameter to support pull-to-refresh.
    - Implemented `handleSuccess` and `handleError` private functions to manage different states of data fetching.
    - Introduced `handleDataUpdateFailedWarning` to specifically manage `DataUpdateFailedWarning` by updating the UI state with a message and showing a Snackbar.
    - Added `onRefreshExchanges` function to trigger a forced data refresh.
    - Updated `getExchanges` to use `collectLatest` for collecting the flow of exchanges.
    - Data fetching is now initiated in the `init` block of the ViewModel.
    - Loading and refreshing states are now handled separately in the UI state.

--- app/src/main/java/br/com/seucaio/cryptoexchanges/ui/screen/ExchangeViewModel.kt
+++ app/src/main/java/br/com/seucaio/cryptoexchanges/ui/screen/ExchangeViewModel.kt
@@ -1,7 +1,10 @@
 package br.com.seucaio.cryptoexchanges.ui.screen
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.seucaio.cryptoexchanges.data.error.DataUpdateFailedWarning
+import br.com.seucaio.cryptoexchanges.domain.model.Exchange
 import br.com.seucaio.cryptoexchanges.domain.usecase.GetExchangesUseCase
 import br.com.seucaio.cryptoexchanges.ui.model.ExchangeDetailsData
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -9,13 +12,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class ExchangeViewModel(
-    private val getExchangesUseCase: GetExchangesUseCase
+    private val getExchangesUseCase: GetExchangesUseCase,
+    val snackBarHostState: SnackbarHostState = SnackbarHostState()
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(ExchangeUiState())
     val uiState: StateFlow<ExchangeUiState> = _uiState
@@ -23,18 +26,56 @@ class ExchangeViewModel(
     private val _uiAction = MutableSharedFlow<ExchangeUiAction>()
     val uiAction: SharedFlow<ExchangeUiAction> = _uiAction
 
-    fun getExchanges() {
+    init {
+        getExchanges()
+    }
+
+    fun getExchanges(forceRefresh: Boolean = false) {
         viewModelScope.launch {
-            getExchangesUseCase()
-                .onStart { _uiState.update { it.setLoading(true) } }
-                .catch { error ->
-                    _uiState.update { it.setError(error = error.message.orEmpty()) }
+            val isPullToRefresh = forceRefresh
+
+            _uiState.update { currentState ->
+                if (isPullToRefresh) {
+                    currentState.setRefreshing(true)
+                } else {
+                    currentState.setLoading(true)
                 }
-                .onCompletion { _uiState.update { it.setLoading(false) } }
-                .collect { exchanges -> _uiState.update { it.setExchanges(exchanges) } }
+            }
+
+            getExchangesUseCase(forceRefresh)
+                .catch { error -> handleError(error) }
+                .collectLatest { exchanges -> handleSuccess(exchanges) }
         }
     }
 
+    private fun handleSuccess(exchanges: List<Exchange>) {
+        _uiState.update { it.setExchanges(exchanges) }
+    }
+
+    private fun handleError(e: Throwable) {
+        if (e is DataUpdateFailedWarning) {
+            handleDataUpdateFailedWarning(e)
+        } else {
+            _uiState.update { it.setError(error = e.message.orEmpty()) }
+        }
+
+    }
+
+    private fun handleDataUpdateFailedWarning(e: Throwable) {
+        val warningMessage = e.message ?: "Não foi possível atualizar os dados."
+        _uiState.update { it.setMessage(warningMessage) }
+        showSnackBar(message = warningMessage)
+    }
+
+    private fun showSnackBar(message: String, actionLabel: String? = null) {
+        viewModelScope.launch {
+            snackBarHostState.showSnackbar(message = message, actionLabel = actionLabel)
+        }
+    }
+
+    fun onRefreshExchanges() {
+        getExchanges(forceRefresh = true)
+    }
 
     fun onItemClicked(uniqueId: String) {
         uiState.value.exchanges
